dbb0e5bbdc71d1a1fb4e05a8d7645d5633694469
unknown <ikfina.kamaliya@gmail.com>
2025-09-26 22:44:14 +0700

    add main

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
-# Load model (.keras)
-@st.cache_resource  # biar model cuma diload sekali
+# Load model
+@st.cache_resource
 def load_model():
     return tf.keras.models.load_model("cnn_tbc_model.keras")
 
 model = load_model()
 
-# Judul Aplikasi
-st.title("Klasifikasi Citra TBC vs Normal")
-st.write("Upload gambar X-ray untuk deteksi **TBC** menggunakan model CNN.")
+st.title("🩻 Klasifikasi Citra X-Ray TBC vs Normal")
+st.write("Upload gambar **X-ray dada** untuk deteksi TBC menggunakan model CNN. \
+Jika gambar bukan X-ray dada, model akan memberi peringatan.")
 
 # Upload gambar
 uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg","jpeg","png"])
@@ -20,26 +20,23 @@ uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg","jpeg","png"])
 if uploaded_file is not None:
     # Preprocessing gambar
     img = image.load_img(uploaded_file, target_size=(64,64))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-    prediction = model.predict(img_array)
-    prob = float(prediction[0][0])  # prob utk kelas index=1 (tuber)
+    # Prediksi
+    prob = float(model.predict(img_array)[0][0])
 
-    confidence = max(prob, 1-prob)  # ambil confidence
-    if prob > 0.5:
+    # Threshold confidence
+    if prob > 0.7:
         label = "TBC"
-    else:
+        st.success(f"✅ Prediksi Model: **{label}** (Probabilitas {prob:.3f})")
+    elif prob < 0.3:
         label = "Normal"
-
-    st.image(img, caption=f"Hasil Prediksi: {label}", use_container_width=True)
-    st.write(f"Probabilitas TBC: {prob:.3f}")
-    st.write(f"Confidence: {confidence:.3f}")
-
-    if confidence < 0.75:
-        st.warning("⚠️ Model tidak yakin. Mungkin gambar bukan chest X-ray atau kualitas data rendah.")
+        st.success(f"✅ Prediksi Model: **{label}** (Probabilitas {prob:.3f})")
     else:
-        if label == "TBC":
-            st.success("✅ Prediksi Model: **TBC**")
-        else:
-            st.success("✅ Prediksi Model: **Normal (non-TBC)**")
+        label = "Unknown"
+        st.warning(f"⚠️ Model tidak yakin (Probabilitas {prob:.3f}). \
+Mungkin gambar bukan X-ray dada atau kualitas gambar buruk.")
+
+    # Tampilkan gambar
+    st.image(img, caption=f"Hasil Prediksi: {label}", use_container_width=True)
